905e50dfe9c4f1537a74a6d88c40955b25b5dad3
tamanna <tamanna35-1411@diu.edu.bd>
2021-06-04 16:56:01 +0600

    [ADDED] email exsists check in apply/views.py

--- apply/models.py
+++ apply/models.py
@@ -10,3 +10,5 @@ class ApplyModel(models.Model):
 
     def __str__(self):
         return self.name
+ 
+
--- apply/views.py
+++ apply/views.py
@@ -3,14 +3,23 @@ from .models import ApplyModel
 # Create your views here.
 
 def home_apply(request):
+    email_error = ''
     if request.method == 'POST':
-        name = request.POST['name']
+    
         email = request.POST['email']
-        profession = request.POST.get('toggle')
-        create_apply_object = ApplyModel.objects.create(name=name,email=email,profession=profession)
-        create_apply_object.save()
+    
+        if ApplyModel.objects.filter(email=email).exists():
+            email_error = 'Email already exsists !!'
+        else:
+            name = request.POST['name']
+            profession = request.POST.get('toggle')
+            create_apply_object = ApplyModel.objects.create(name=name,email=email,profession=profession)
+            create_apply_object.save()
         
     template_name = 'index.html'
-    context = {}
-
+    context = {'error': email_error }
     return render(request,template_name,context)
+
+
+
+
